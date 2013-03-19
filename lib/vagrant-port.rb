d9c53dd76092b46d5d1f138560bc12ca3692103c
Jordan Sissel <jls@semicomplete.com>
2013-03-18 22:35:40 -0700

    vagrant requires this file

---
+++ lib/vagrant-port.rb
@@ -0,0 +1,6 @@
+# This file is required because Vagrant's plugin system expects
+# a eponymous ruby file matching the rubygem.
+#
+# So this gem is called 'vagrant-port' and thus vagrant tries
+# to require "vagrant-port"
+require "vagrant-port/port"
